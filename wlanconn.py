fdf1b59e653ec911f5f57151130192e09cd7fb2a
LoneMarvel <tinost@gmail.com>
2020-02-24 18:04:44 +0100

    Updated version

--- wlanconn.py
+++ wlanconn.py
@@ -6,37 +6,25 @@ from getpass import getpass
 
 
 def DoConn(wifiIn):
-    passwdFlag = True    
+    passwdFlag = True         
     historyList = subprocess.getoutput("ls /etc/sysconfig/network-scripts/keys* | awk -F'/' '{print $5}'")  
     keyId = historyList.split('\n')  
     for i in range(len(keyId)):        
-        if str(keyId[i])[5:] == str(wlanArr[wifiIn])[2:-2]:            
+        if str(strTxt[wifiIn]) in str(keyId[i])[5:]:                   
             passwdFlag = False
             break
 
     if passwdFlag == True:        
         passwd = getpass(prompt='Type Your Password -> ')
-        wifiConn = subprocess.getoutput(f'nmcli dev wifi connect {str(wlanArr[wifiIn])[2:-2]} password {passwd}')
+        subprocess.getoutput(f'nmcli dev wifi connect {str(strTxt[wifiIn])} password {passwd}')
     else:        
-        wifiConn = subprocess.getoutput(f'nmcli connection up {str(keyId[i])[5:]}')    
+        subprocess.getoutput(f'nmcli connection up {str(strTxt[wifiIn])}')    
     
 
-strTxt = ''
-k = 0
-wlanArr = []
-
 wlanList = subprocess.getoutput("nmcli dev wifi list | awk '{print $1}'")
+strTxt = wlanList.split('\n')
+for i in range(len(strTxt)):
+    print(f'ID# -> {i} Network SSID -> {str(strTxt[i])}')
 
-for i in range(len(wlanList)):
-    strTxt += wlanList[i]
-    if wlanList[i] == '\n':                
-        wlanArr.append(strTxt.splitlines())
-        strTxt = ''
-        k += 1
-
-del wlanArr[0]
-for i in range(len(wlanArr)):
-    print(f'{i} -> {str(wlanArr[i])[2:-2]}')   
-
-wifiIn = int(input('Connect In WiFi Network ( 99 to Exit ) -> '))
+wifiIn = int(input('Connect To A WiFi Network Using ID ( 99 to Exit ) -> '))
 sys.exit if wifiIn == 99 else DoConn(wifiIn)
